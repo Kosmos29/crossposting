78948d00ce56e2b77efba5850ad5163fb1673ce7
Andrey Kirpichnikov <andry.krp@gmail.com>
2014-04-10 23:10:57 +0600

    datetime joda fix

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,5 +1,4 @@
 import org.joda.time.DateTime
-import org.joda.time.LocalDate
 import org.octocode.Project
 
 class BootStrap {
@@ -25,8 +24,8 @@ class BootStrap {
             new Project(
                     name: "project (${i})",
                     description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                    created: LocalDate.now().minusDays(count - i),
-                    modified: LocalDate.now().plusDays(i)
+                    created: DateTime.now().minusDays(count - i),
+                    modified: DateTime.now().plusDays(i)
             ).save(failOnError: true, flush: true)
         }
     }
--- grails-app/domain/org/octocode/Project.groovy
+++ grails-app/domain/org/octocode/Project.groovy
@@ -1,19 +1,18 @@
 package org.octocode
 
-import org.jadira.usertype.dateandtime.joda.PersistentLocalDate
-import org.joda.time.LocalDate
+import org.joda.time.DateTime
 
 class Project {
     String name
     String description
 
-    LocalDate created
-    LocalDate modified
+    DateTime created
+    DateTime modified
 
     static constraints = {
         name nullable: false, blank: false
         description nullable: true, blank: false,  maxSize: 4096
-        created nullable: false, type: PersistentLocalDate
-        modified nullable: false, type: PersistentLocalDate
+        created nullable: false
+        modified nullable: false
     }
 }
